d95884ad0411429567fdf95cff6e332ec18ef322
NightMareRahul <nightmarecoding0001@gmail.com>
2024-02-06 13:55:20 +0530

    Implemented Integration

--- etm-system/src/App.js
+++ etm-system/src/App.js
@@ -1,35 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CurrLocation from "./FirstPageComponent/CurrLocation";
 import Searchbar from "./FirstPageComponent/Searchbar";
 import Button from "./Button";
 import "./App.css";
 import ShowFootage from "./SecondPageComponent/ShowFootage";
 import Incidents from "./ThirdPageComponent/Incidents";
+import axios from "axios";
 
 export default function App() {
-  // const [data, setData] = useState({});
-  // const note = "test";
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch("http://localhost:5000/api/data");
-  //     const jsonData = await response.json();
-  //     setData(jsonData.message);
-  //     console.log(jsonData);
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   }
-  // };
+  const [message, setMessage] = useState("");
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/data")
+      .then((res) => {
+        setMessage(res.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
 
   return (
     <>
       <div className="mainDiv">
-        <h2>Traffic Control System</h2>
+        <h2>Traffic Control System: </h2>
         <CurrLocation />
-        <h2>Select Area :</h2>
+        <h2>Select Area : </h2>
         <Searchbar />
         <Button content="Check Status" />
       </div>
