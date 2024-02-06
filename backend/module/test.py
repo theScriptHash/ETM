0adc3023934e4deb4ad6124df4703c6e9bbfccc6
NightMareRahul <nightmarecoding0001@gmail.com>
2024-02-06 11:57:07 +0530

    Upgraded model

--- backend/module/test.py
+++ backend/module/test.py
@@ -1,19 +1,18 @@
 import cv2
-# from tracker import *
 
 cap = cv2.VideoCapture("video2.mp4")
 
 # vehicle detector
-vehicle_detector = cv2.createBackgroundSubtractorMOG2()
+vehicle_detector = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=40)
 
 
 while True:
     ret, frame = cap.read()
 
-    # diamention = frame.shape
+    height, width, _ = frame.shape
 
     # region
-    roi = frame[140: 720, 300: 800]
+    # roi = frame[40: 720, 100: 800]
 
     # detecting object
     mask = vehicle_detector.apply(frame)
@@ -24,14 +23,14 @@ while True:
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 200:
-            cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 1)
+        if area > 180:
+            # cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 1)
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # cv2.imshow("roi", roi)
     cv2.imshow("Frame", frame)
-    cv2.imshow("mask", mask)
+    # cv2.imshow("mask", mask)
 
     key = cv2.waitKey(30)
 
